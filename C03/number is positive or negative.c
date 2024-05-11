d1e6632e8df2d2b998e9362c3fa7b510bad29fb5
GURUPRASANTH D <147241556+jackiestroffguru@users.noreply.github.com>
2024-05-11 09:30:35 +0530

    Create number is positive or negative.c

---
+++ C03/number is positive or negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    if (num > 0)    {
+        printf("%d is an Positive number", num);
+    }
+    else{
+        printf("%d is a Negative number", num);
+    }
+    return 0;
+
+}

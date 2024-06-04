bd8c31e20904163cd9f2cad65784fc6a3e0cb77e
GURUPRASANTH D <147241556+guruprasanth02@users.noreply.github.com>
2024-06-04 09:18:31 +0530

    Create Right angle Triangle incremented by 1.c

---
+++ C25/Right angle Triangle incremented by 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i,j, rows, k = 1;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("----RIGHT ANGLE TRIANGLE----\n");
+    for (i = 1;i <= rows;i++) {
+        for(j = 1; j <= i; j++)
+            printf("%d ", k++);
+        printf("\n");
+    }
+    return 0;
+}

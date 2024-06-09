4d0941532f4eff6e3a49f83f3e0946574a74cd81
GURUPRASANTH D <147241556+guruprasanth02@users.noreply.github.com>
2024-06-09 22:49:58 +0530

    Create Pyramid pattern with num repeatation.c

---
+++ C30/Pyramid pattern with num repeatation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+   int rows, tot;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   tot = rows + 4 - 1;
+
+   for(int i = 0; i <= rows; i++) {
+        for(int j = tot; j >= 1; j--) {
+            printf(" ");
+        }
+        for(int k = 1; k <= i; k++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+        tot--;
+   }
+   return 0;
+}

65166e9e1c65ee31f674d262815f63667e267fa0
GURUPRASANTH D <147241556+guruprasanth02@users.noreply.github.com>
2024-11-02 21:00:57 +0530

    Update Product of digits.c

--- C61/Product of digits.c
+++ C61/Product of digits.c
@@ -1,41 +1,29 @@
 #include<stdio.h>
 
-int main() { 
-
-    int num, product, rem;
-
+int main() {
+    int num, product = 1, rem;
     printf("Enter the number: ");
-
     scanf("%d", &num);
-
-    if(num == 0) {
-
-        printf("Invalid Input"); 
-
+    
+    if (num == 0) {
+        printf("Invalid Input.");
         return 0;
-
     }
-
+    
+    if (num < 0) {
+        num = -num;
+    }
+    
     while(num != 0) {
-
         rem = num % 10;
-
-        if(rem == 0) {
-
-            printf("Invalid Input. ");
-
+        if (rem == 0) {
+            printf("Invalid Input.");
             return 0;
-
         }
-
         product = product * rem;
-
         num = num / 10;
-
     }
-
     printf("%d", product);
-
     return 0;
-
 }
+

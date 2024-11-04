1ab581d61f168f431b50f36a0051443010b40238
GURUPRASANTH D <147241556+guruprasanth02@users.noreply.github.com>
2024-11-04 14:05:23 +0530

    Update Sum of prime factors.c

--- C63/Sum of prime factors.c
+++ C63/Sum of prime factors.c
@@ -1,58 +1,34 @@
 #include <stdio.h>
 
 int main() {
+    int num, sum = 0, org;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
 
-int num, sum = 0, org;
+    if (num < 1) {
+        printf("The number should be a positive integer.\n");
+        return 1;
+    }
 
-printf("Enter an integer: ");
+    org = num;
 
-scanf("%d", &num);
-
-if (num < 1) {
-
-printf("The number should be a positive integer.\n");
-
-return 1;
-
+    if (num % 2 == 0) {
+        sum = sum + 2;
+        while (num % 2 == 0) {
+            num = num / 2;
+        }
+    }
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0) {
+            sum = sum + i;
+            while (num % i == 0) {
+                num = num / i;
+            }
+        }
+    }
+    if (num > 2) {
+        sum =  sum + num;
+    }
+    printf("The sum of prime factors: %d", sum);
+    return 0;
 }
-
-org = num;
-
-if (num % 2 == 0) {
-
-sum = sum + 2;
-
-while (num % 2 == 0) {
-
-num = num / 2;
-
-}
-
-}
-
-for (int i = 3; i * i <= num; i += 2) {
-
-if (num % i == 0) {sum = sum + i;
-
-while (num % i == 0) {
-
-num = num / i;
-
-}
-
-}
-
-}
-
-if (num > 2) {
-
-sum = sum + num;
-
-}
-
-printf("%d", sum);
-
-return 0;
-
-}
-
